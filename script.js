790a5e40e9c9f051bdd6d04b9625356ef9912588
TallerThanShort <tallerthanshort@noreply.codeberg.org>
2022-11-17 19:15:04 +0000

    Feat: pin, very and modal
    Signed-off-by: TallerThanShort <tallerthanshort@noreply.codeberg.org>

--- script.js
+++ script.js
@@ -8,14 +8,16 @@ const liveReload = (data) => {
         const rt = doc.data();
         const update = `
         <br>
-        <div style="border: 3px solid; position: realative; left: 0;">
+        <div style="border: 3px solid; position: realative; right: 0;">
             <div style="position: relative; top: 6px; left: 10px; width: 100%;">
-                <img src="${rt.image}" style="position: relative; top: 10px; left: 12px; width: 32px; height: 32px;">
-                <p onclick="location.href='/u/?u=${rt.handle}';" style="width: 50%; cursor: pointer; position: relative; top: -40px; left: 50px;">${rt.username}</p>
-                <p onclick="location.href='/u/?u=${rt.handle}';" style="width: 50%; color: gray; font-size: 10px; position: relative; top: -49px; left: 52px; cursor: pointer;">@${rt.handle}</p>
+                <img src="${rt.image}" style="position: relative; top: 10px; left: 12px; width: 32px; height: 32px; border-radius: 50%;">
+                <p onclick="location.href='/u/?u=${rt.handle}';" style="width: 10%; cursor: pointer; position: relative; top: -40px; left: 51px;">${rt.username}</p>
+                <p onclick="location.href='/u/?u=${rt.handle}';" style="width: 10%; color: gray; font-size: 10px; position: relative; top: -49px; left: 52px; cursor: pointer;">@${rt.handle}</p>${rt.very}
+                <p style="width: 8%; color: gray; position: relative; top: -90px; left: 78%;">${rt.date}</p>
             </div>
+            ${rt.pin}
             <div style="position: relative;">
-                <p style="width: 60%; cursor: default; position: relative; left: 10px;">${rt.content}</p>
+                <p style="width: 86.6%; cursor: default; position: relative; left: 10px;">${rt.content}</p>
             </div>
         </div>
         `;
@@ -24,3 +26,13 @@ const liveReload = (data) => {
     document.getElementById("feed").innerHTML = html;
 }
 
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    var modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+  
+    var items = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(items);
+  
+  });
