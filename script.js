6745f7c48d7df4c374272fd3f296707a2cb8d670
TallerThanShort <tallerthanshort@noreply.codeberg.org>
2022-11-21 17:40:14 +0000

    finish Fix XSS at1
    This removes XSS vulnerabilities from the site by forcibly spoon-feeding the back-end HTML Entities rather than HTML Tags
    
    Signed-off-by: TallerThanShort <tallerthanshort@noreply.codeberg.org>

--- script.js
+++ script.js
@@ -20,11 +20,10 @@ const liveReload = (data) => {
         <div style="border: 3px solid; position: realative; right: 0;">
             <div style="position: relative; top: 6px; left: 10px; width: 100%;">
                 <img src="${rt.image}" style="position: relative; top: 10px; left: 12px; width: 32px; height: 32px; border-radius: 50%;">
-                <p onclick="location.href='/u/?u=${rt.handle}';" style="width: 10%; cursor: pointer; position: relative; top: -40px; left: 51px;">${rt.username}</p>
+                <p id="usernamezd1" onclick="location.href='/u/?u=${rt.handle}';" style="width: 10%; cursor: pointer; position: relative; top: -40px; left: 51px;">${rt.username}</p>
                 <p onclick="location.href='/u/?u=${rt.handle}';" style="width: 10%; color: gray; font-size: 10px; position: relative; top: -49px; left: 52px; cursor: pointer;">@${rt.handle}</p>${rt.very}
                 <p style="width: 8%; color: gray; position: relative; top: -90px; left: 78%;">${rt.date}</p>
             </div>
-            ${rt.pin}
             <div style="position: relative;">
                 <pre style="width: 86.6%; cursor: default; position: relative; left: 10px;">${rt.content}</pre>
             </div>
