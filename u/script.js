07e91fb7e157fb5d214789195d1d878ab2541bcd
TallerThanShort <tallerthanshort@noreply.codeberg.org>
2022-11-23 17:00:43 +0000

    white-space: pre-wrap on /u
    Signed-off-by: TallerThanShort <tallerthanshort@noreply.codeberg.org>

--- u/script.js
+++ u/script.js
@@ -30,7 +30,7 @@ if(nuser != null){
                     <p style="width: 8%; color: gray; position: relative; top: -90px; left: 78%;">${nob.date}</p>
                 </div>
                 <div style="position: relative;">
-                    <pre style="width: 86.6%; cursor: default; position: relative; left: 10px;">${nob.content}</pre>
+                    <pre style="width: 86.6%; cursor: default; position: relative; left: 10px; white-space: pre-wrap;">${nob.content}</pre>
                 </div>
                 <div class="logged-in" style="position: relative; overflow: hidden;" id="fancy-icons">
                     <div style="float: left; display: block; text-align: center; padding: 14px; text-decoration: none; cursor: pointer;"><img src="https://firebasestorage.googleapis.com/v0/b/shnitters.appspot.com/o/reply.png?alt=media&token=5124acf4-60f7-4f53-bc6e-4803b534352d&format=webp" style="height: 18px; width: 18px;"> Reply</div>
