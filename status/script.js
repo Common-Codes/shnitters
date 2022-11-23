9165407bcad4ba2c4c15c96ab1b9c12ff0c809d9
TallerThanShort <tallerthanshort@noreply.codeberg.org>
2022-11-23 17:03:37 +0000

    word wrap or whatever the hell it was
    also removed a really long commented out section
    
    Signed-off-by: TallerThanShort <tallerthanshort@noreply.codeberg.org>

--- status/script.js
+++ status/script.js
@@ -22,7 +22,7 @@ if(post != null){
                 <p style="width: 8%; color: gray; position: relative; top: -90px; left: 78%;">${leg.date}</p>
             </div>
             <div style="position: relative;">
-                <pre style="width: 86.6%; cursor: default; position: relative; left: 10px;">${leg.content}</pre>
+                <pre style="width: 86.6%; cursor: default; position: relative; left: 10px; white-space: pre-wrap;">${leg.content}</pre>
             </div>
             <div class="logged-in" style="position: relative; overflow: hidden;" id="fancy-icons">
                 <div style="float: left; display: block; text-align: center; padding: 14px; text-decoration: none; cursor: pointer;"><img src="https://firebasestorage.googleapis.com/v0/b/shnitters.appspot.com/o/reply.png?alt=media&token=5124acf4-60f7-4f53-bc6e-4803b534352d&format=webp" style="height: 18px; width: 18px;"> Reply</div>
@@ -32,28 +32,6 @@ if(post != null){
         </div>
         `;
     });
-
-    /*store.collection("status").where("handle", "==", post).get().then((querySnaphot) => {
-        querySnaphot.forEach((doc) => {
-            const nob = doc.data();
-            const statuses = `
-            <br>
-            <div style="border: 3px solid; position: realative; right: 0;">
-                <div style="position: relative; top: 6px; left: 10px; width: 100%;">
-                    <img src="${nob.image}" style="position: relative; top: 10px; left: 12px; width: 32px; height: 32px; border-radius: 50%;">
-                    <p onclick="location.href='/u/?u=${nob.handle}';" style="width: 10%; cursor: pointer; position: relative; top: -40px; left: 51px;">${nob.username}</p>
-                    <p onclick="location.href='/u/?u=${nob.handle}';" style="width: 10%; color: gray; font-size: 10px; position: relative; top: -49px; left: 52px; cursor: pointer;">${nob.handle}</p>${nob.very}
-                    <p style="width: 8%; color: gray; position: relative; top: -90px; left: 78%;">${nob.date}</p>
-                </div>
-                <div style="position: relative;">
-                    <pre style="width: 86.6%; cursor: default; position: relative; left: 10px;  word-break: break-word;">${nob.content}</pre>
-                </div>
-            </div>
-        `;
-
-        document.getElementById("pistons").innerHTML += statuses;
-        })
-    });*/
 }
 
 const setupUI = (user) => {
