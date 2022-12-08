d428bedc8b9e047c87036e52d7228ae33906f12f
TallerThanShort <tallerthanshort@noreply.codeberg.org>
2022-12-08 18:03:59 +0000

    fix action buttons
    Signed-off-by: TallerThanShort <tallerthanshort@noreply.codeberg.org>

--- u/script.js
+++ u/script.js
@@ -13,7 +13,13 @@ if(nuser != null){
         querySnaphot.forEach((doc) => {
             const pro = doc.data();
             document.title = `${pro.handle} | Shnitters`
-            document.getElementById("profile").innerHTML += `<img src="${pro.pfp}" style="position: relative; top: 10px; left: 12px; width: 64px; height: 64px;"><b style="width: 50%; cursor: default; position: relative; top: -36px; left: 40px;">${pro.username}</b><p style="width: 50%; color: gray; font-size: 10px; position: relative; top: -48px; left: 101px;">${pro.handle}</p>`
+            document.getElementById("profile").innerHTML += `
+            <img src="${pro.pfp}" style="position: relative; top: 10px; left: 12px; width: 64px; height: 64px;">
+            <b style="width: 50%; cursor: default; position: relative; top: -36px; left: 36px;">${pro.username}</b>
+            <p style="width: 50%; color: gray; font-size: 10px; position: relative; top: -48px; left: 101px;">${pro.handle}</p>
+            <p id="flowzer" style="width: 50%; color: gray; position relative; top: -48px; left: 101px;">${pro.followers} Followers</p>
+            <button style="text-transform: uppercase; border: none; background-color: blue; color: white; width: 84px; height: 23px; font-weight: 550; cursor: pointer; position: relative; top: -93px; left: 69%;">follow</button>
+            `
         })
     });
 
@@ -30,11 +36,11 @@ if(nuser != null){
                     <p style="width: 8%; color: gray; position: relative; top: -90px; left: 78%;">${nob.date}</p>
                 </div>
                 <div style="position: relative;">
-                    <pre style="width: 86.6%; cursor: default; position: relative; left: 10px; white-space: pre-wrap;">${nob.content}</pre>
+                    <pre style="width: 86.6%; cursor: default; position: relative; left: 10px;">${nob.content}</pre>
                 </div>
                 <div class="logged-in" style="position: relative; overflow: hidden;" id="fancy-icons">
-                    <div style="float: left; display: block; text-align: center; padding: 14px; text-decoration: none; cursor: pointer;"><img src="https://firebasestorage.googleapis.com/v0/b/shnitters.appspot.com/o/reply.png?alt=media&token=5124acf4-60f7-4f53-bc6e-4803b534352d&format=webp" style="height: 18px; width: 18px;"> Reply</div>
-                    <div style="float: left; display: block; text-align: center; padding: 14px; text-decoration: none; cursor: pointer;"><img src="https://firebasestorage.googleapis.com/v0/b/shnitters.appspot.com/o/like.png?alt=media&token=b212b954-864b-4e5f-9258-3f234863c3d6&format=webp" style="height: 18px; width: 18px;"> Love</div>
+                    <div style="float: left; display: block; text-align: center; padding: 14px; text-decoration: none; cursor: pointer;" onclick="location.href='/status/?p=${doc.id}';"><img src="https://firebasestorage.googleapis.com/v0/b/shnitters.appspot.com/o/reply.png?alt=media&token=5124acf4-60f7-4f53-bc6e-4803b534352d&format=webp" style="height: 18px; width: 18px;"> Reply</div>
+                    <div style="float: left; display: block; text-align: center; padding: 14px; text-decoration: none; cursor: pointer;" onclick="location.href='/status/?p=${doc.id}';"><img src="https://firebasestorage.googleapis.com/v0/b/shnitters.appspot.com/o/like.png?alt=media&token=b212b954-864b-4e5f-9258-3f234863c3d6&format=webp" style="height: 18px; width: 18px;"> Love</div>
                     <div style="float: left; display: block; text-align: center; padding: 14px; text-decoration: none; cursor: pointer;" onclick="location.href='/status/?p=${doc.id}';"><img src="https://firebasestorage.googleapis.com/v0/b/shnitters.appspot.com/o/comment.png?alt=media&token=aef99c51-74c5-4482-9233-ce7524c3dfbc&format=webp" style="height: 18px; width: 18px;"> Conversation</div>
                 </div>
             </div>
