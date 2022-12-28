6e48cabffa1d8ca5a910e0a897e15d6c63418263
TallerThanShort <tallerthanshort@noreply.codeberg.org>
2022-12-28 14:45:59 +0000

    Add live tweet updater
    Automatically add tweets to the top of the feed without refreshing browser
    
    Signed-off-by: TallerThanShort <tallerthanshort@noreply.codeberg.org>

--- script.js
+++ script.js
@@ -7,37 +7,40 @@ let xyzname = ``
 let veryvery = ``
 
 
-store.collection('status').get().then(snapshot => {
-    liveReload(snapshot.docs);
-});
+var tweetsRef = firebase.firestore().collection('status');
 
-const liveReload = (data) => {
-    let html = ``;
-    data.forEach(doc => {
-        const rt = doc.data();
-        const update = `
-        <br>
+var query = tweetsRef.orderBy('timestamp', 'desc');
+
+query.onSnapshot(function(snapshot) {
+  var tweets = snapshot.docs.map(function(tweet) {
+    return tweet.data();
+  });
+
+  document.getElementById('tweets').innerHTML = '';
+
+  tweets.forEach(function(tweet) {
+    var tweetElement = `
+    <br>
         <div style="border: 3px solid; position: realative; right: 0;">
             <div style="position: relative; top: 6px; left: 10px; width: 100%;">
-                <img src="${rt.image}" style="position: relative; top: 10px; left: 12px; width: 32px; height: 32px; border-radius: 50%;">
-                <p onclick="location.href='/u/?u=${rt.handle}';" style="width: 10%; cursor: pointer; position: relative; top: -40px; left: 51px;">${rt.username}</p>
-                <p onclick="location.href='/u/?u=${rt.handle}';" style="width: 10%; color: gray; font-size: 10px; position: relative; top: -49px; left: 52px; cursor: pointer;">${rt.handle}</p>${rt.very}
-                <p style="width: 8%; color: gray; position: relative; top: -90px; left: 78%;">${rt.date}</p>
+                <img src="${tweet.image}" style="position: relative; top: 10px; left: 12px; width: 32px; height: 32px; border-radius: 50%;">
+                <p onclick="location.href='/u/?u=${tweet.handle}';" style="width: 10%; cursor: pointer; position: relative; top: -40px; left: 51px;">${tweet.username}</p>
+                <p onclick="location.href='/u/?u=${tweet.handle}';" style="width: 10%; color: gray; font-size: 10px; position: relative; top: -49px; left: 52px; cursor: pointer;">${tweet.handle}</p>${tweet.very}
+                <p style="width: 8%; color: gray; position: relative; top: -90px; left: 78%;">${tweet.date}</p>
             </div>
             <div style="position: relative;">
-                <pre style="width: 86.6%; cursor: default; position: relative; left: 10px; white-space: pre-wrap;">${rt.content}</pre>
+                <pre style="width: 86.6%; cursor: default; position: relative; left: 10px; white-space: pre-wrap;">${tweet.content}</pre>
             </div>
             <div class="logged-in" style="position: relative; overflow: hidden;" id="fancy-icons">
-                <div style="float: left; display: block; text-align: center; padding: 14px; text-decoration: none; cursor: pointer;" onclick="location.href='/status/?p=${doc.id}';"><img src="https://firebasestorage.googleapis.com/v0/b/shnitters.appspot.com/o/reply.png?alt=media&token=5124acf4-60f7-4f53-bc6e-4803b534352d&format=webp" style="height: 18px; width: 18px;"> Reply</div>
-                <div style="float: left; display: block; text-align: center; padding: 14px; text-decoration: none; cursor: pointer;" onclick="location.href='/status/?p=${doc.id}';"><img src="https://firebasestorage.googleapis.com/v0/b/shnitters.appspot.com/o/like.png?alt=media&token=b212b954-864b-4e5f-9258-3f234863c3d6&format=webp" style="height: 18px; width: 18px;"> Love</div>
-                <div style="float: left; display: block; text-align: center; padding: 14px; text-decoration: none; cursor: pointer;" onclick="location.href='/status/?p=${doc.id}';"><img src="https://firebasestorage.googleapis.com/v0/b/shnitters.appspot.com/o/comment.png?alt=media&token=aef99c51-74c5-4482-9233-ce7524c3dfbc&format=webp" style="height: 18px; width: 18px;"> Conversation</div>
+                <div style="float: left; display: block; text-align: center; padding: 14px; text-decoration: none; cursor: pointer;" onclick="location.href='/status/?p=${tweet.id}';"><img src="https://firebasestorage.googleapis.com/v0/b/shnitters.appspot.com/o/reply.png?alt=media&token=5124acf4-60f7-4f53-bc6e-4803b534352d&format=webp" style="height: 18px; width: 18px;"> Reply</div>
+                <div style="float: left; display: block; text-align: center; padding: 14px; text-decoration: none; cursor: pointer;" onclick="location.href='/status/?p=${tweet.id}';"><img src="https://firebasestorage.googleapis.com/v0/b/shnitters.appspot.com/o/like.png?alt=media&token=b212b954-864b-4e5f-9258-3f234863c3d6&format=webp" style="height: 18px; width: 18px;"> Love</div>
+                <div style="float: left; display: block; text-align: center; padding: 14px; text-decoration: none; cursor: pointer;" onclick="location.href='/status/?p=${tweet.id}';"><img src="https://firebasestorage.googleapis.com/v0/b/shnitters.appspot.com/o/comment.png?alt=media&token=aef99c51-74c5-4482-9233-ce7524c3dfbc&format=webp" style="height: 18px; width: 18px;"> Conversation</div>
             </div>
         </div>
-        `;
-        html += update;
-    });
-    document.getElementById("feed").innerHTML = html;
-}
+    `;
+    document.getElementById('tweets').innerHTML += (tweetElement);
+  });
+});
 
 const setupUI = (user) => {
     if(user){
